6c674a5bf234cf25d5d351f915b627d6962ca028
Greg Mills <gmills@magento.com>
2016-09-27 09:00:17 -0400

    fixed model typo

--- Model/CmsBlock.php
+++ Model/CmsBlock.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\WidgetSampleData\Model;
+namespace MagentoEse\B2bWidgetSampleData\Model;
 
 use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
 
